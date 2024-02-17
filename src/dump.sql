7ee2ec925038a12a322743b83dddc79c3d1ac9f4
Bárbara Lourenço Tavares <babisolua@gmail.com>
2024-02-17 19:55:15 -0300

    conexao com banco de dados e schema sql

---
+++ src/dump.sql
@@ -0,0 +1,43 @@
+create database dindin; 
+
+create table usuarios (
+  id serial primary key, 
+  nome text not null,
+  email text unique,
+  senha text unique 
+  );
+
+create table categorias (
+  id serial primary key, 
+  descricao text not null
+  );
+  
+  create table transacoes (
+    id serial primary key, 
+    descricao text, 
+    valor integer, 
+    data timestamp default now(),
+    categoria_id integer references categorias(id), 
+    usuario_id integer references usuarios(id),
+    tipo text not null
+    );
+    
+
+insert into categorias(descricao) values 
+('Alimentação'), 
+('Assinaturas e Serviços'),
+('Casa'), 
+('Mercado'),
+('Cuidados Pessoais'), 
+('Educação'), 
+('Família'), 
+('Lazer'), 
+('Pets'), 
+('Presentes'), 
+('Roupas'), 
+('Saúde'), 
+('Transporte'), 
+('Salário'), 
+('Vendas'), 
+('Outras receitas'), 
+('Outras despesas');
